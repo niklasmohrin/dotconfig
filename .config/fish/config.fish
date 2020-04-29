62630a0068fd37544273d7d4251bb7755443f05e
Niklas Mohrin <niklas.mohrin@gmail.com>
2020-04-29 18:33:44 +0200

    adds fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,7 @@
+set PATH $PATH $HOME/.cargo/bin
+
+
+alias exa='exa --all --long --git --classify'
+alias vim=nvim
+alias dotfilecfg='git --git-dir=/home/niklas/dotfiles/ --work-tree=/home/niklas'
+
