3fcfa2c2a5770721b523876081e72e882ac80112
Niklas Mohrin <dev@niklasmohrin.de>
2021-04-17 17:36:43 +0200

    latex helpers

--- .config/nvim/lua/niklas/general_config.lua
+++ .config/nvim/lua/niklas/general_config.lua
@@ -17,8 +17,8 @@ vim.wo.list = true
 vim.wo.listchars = "trail:·,tab:>-"
 
 -- local to buffer
-vim.o.tabstop = 4
+vim.bo.tabstop = 4
 vim.o.shiftwidth = 4
-vim.o.expandtab = true
+vim.bo.expandtab = true
 vim.o.undofile = true
 vim.o.spelllang = "en_us,de_de"
