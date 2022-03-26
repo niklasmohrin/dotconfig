4756b11b69ede2f2be874fb4812e4cc4bb35466c
Niklas Mohrin <dev@niklasmohrin.de>
2022-03-26 17:31:05 +0100

    Add script to build nvim with bundled dependencies

---
+++ .config/nvim/cleanbuild.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -ex
+
+# Note:
+# For bundled dependencies,
+#   https://aur.archlinux.org/packages/neovim-git
+# is a good place to look. In particular, I found that libvterm 0.2 is
+# not supported, and installed the libvterm-0.1 package from the AUR.
+
+make distclean
+rm -rf build
+
+git pull --ff-only
+
+mkdir build
+cd build
+cmake -G Ninja -D CMAKE_BUILD_TYPE=RelWithDebInfo ..
+ninja
+sudo ninja install
