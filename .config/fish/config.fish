ef2735368d43d97b7272711b8477537dc0f0f976
Niklas Mohrin <dev@niklasmohrin.de>
2023-09-17 19:44:14 +0200

    update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,7 +7,7 @@ set -x PATH \
     $HOME/go/bin \
     $HOME/.local/bin \
     $HOME/.local/share/yarn/global/node_modules/.bin \
-    $HOME/Uni/SoSe21/CompProg/toolbin \
+    $HOME/Dev/cp/toolbin \
     /usr/bin/site_perl \
     /usr/bin/vendor_perl \
     /usr/bin/core_perl
@@ -17,6 +17,8 @@ set -x EDITOR nvim
 set -x MANPAGER "nvim +Man!"
 set -x XDG_DATA_HOME "$HOME/.local/share"
 set -x GPG_TTY (tty)
+
+set -x CARGO_TARGET_DIR "$HOME/.cache/cargo-target-dir/"
 
 if status is-interactive
     fish_vi_key_bindings
@@ -29,11 +31,10 @@ if status is-interactive
 
     abbr cal "cal -m"
     abbr cc "g++ -std=c++20 -Wall -Wextra"
-    abbr exa exa -lahF
+    abbr l eza -lahF
     abbr fishcfg "nvim ~/.config/fish/config.fish"
     abbr open xdg-open
     abbr qtilecfg "nvim ~/.config/qtile/config.py"
-    abbr triple-monitor 'xrandr --output eDP-1 --auto --output DP-3 --primary --auto --right-of eDP-1 --output DP-1 --auto --right-of DP-3'
     abbr vim nvim
     abbr xclip "xclip -sel clip"
 
@@ -54,6 +55,40 @@ if status is-interactive
     abbr gs "git status"
     abbr gsh "git show"
     abbr gsw "git switch"
+
+    # Kanagawa Fish shell theme
+    set -l foreground DCD7BA
+    set -l selection 2D4F67
+    set -l comment 727169
+    set -l red C34043
+    set -l orange FF9E64
+    set -l yellow C0A36E
+    set -l green 76946A
+    set -l purple 957FB8
+    set -l cyan 7AA89F
+    set -l pink D27E99
+
+    # Syntax Highlighting Colors
+    set -g fish_color_normal $foreground
+    set -g fish_color_command $cyan
+    set -g fish_color_keyword $pink
+    set -g fish_color_quote $yellow
+    set -g fish_color_redirection $foreground
+    set -g fish_color_end $orange
+    set -g fish_color_error $red
+    set -g fish_color_param $purple
+    set -g fish_color_comment $comment
+    set -g fish_color_selection --background=$selection
+    set -g fish_color_search_match --background=$selection
+    set -g fish_color_operator $green
+    set -g fish_color_escape $pink
+    set -g fish_color_autosuggestion $comment
+
+    # Completion Pager Colors
+    set -g fish_pager_color_progress $comment
+    set -g fish_pager_color_prefix $cyan
+    set -g fish_pager_color_completion $foreground
+    set -g fish_pager_color_description $comment
 end
 
 set fish_greeting
