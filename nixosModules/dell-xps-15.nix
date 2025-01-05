124ecd46525d0aa619f870f9222f198ea7081ead
Niklas Mohrin <dev@niklasmohrin.de>
2025-01-05 21:13:24 +0100

    update to 24.11

--- nixosModules/dell-xps-15.nix
+++ nixosModules/dell-xps-15.nix
@@ -28,11 +28,7 @@ in
   services.blueman.enable = true;
 
   hardware = {
-    opengl = {
-      enable = true;
-      driSupport = true;
-      driSupport32Bit = true;
-    };
+    graphics.enable = true;
     nvidia = {
       modesetting.enable = true;
       open = true;
@@ -40,11 +36,12 @@ in
       package = config.boot.kernelPackages.nvidiaPackages.stable;
     };
   };
+  services.xserver.videoDrivers = [ "nvidia" ];
 
   nixpkgs.config = {
     allowUnfreePredicate = pkg:
       builtins.elem pkg.meta.license.shortName [ "CUDA EULA" "CUDA TOOLKIT" ] ||
-      builtins.elem (lib.getName pkg) [ "nvidia-settings" "nvidia-x11" "steam" "steam-original" "steam-run" ];
+      builtins.elem (lib.getName pkg) [ "nvidia-settings" "nvidia-x11" "steam" "steam-unwrapped" "steam-original" "steam-run" ];
   };
 
   environment.systemPackages = with pkgs; [
