32700088c0c99b07a0b89e7ef1ae48eb78a4345d
Niklas Mohrin <dev@niklasmohrin.de>
2025-05-25 22:56:29 +0200

    read more plugins

--- .config/nvim/lua/niklas/lazy-plugins/completion.lua
+++ .config/nvim/lua/niklas/lazy-plugins/completion.lua
@@ -3,12 +3,11 @@ return {
         "hrsh7th/nvim-cmp",
         commit = "059e89495b3ec09395262f16b1ad441a38081d04",
         dependencies = {
-            "hrsh7th/cmp-cmdline",
             { "hrsh7th/cmp-buffer", commit = "b74fab3656eea9de20a9b8116afa3cfc4ec09657" },
-            "hrsh7th/cmp-calc",
-            "hrsh7th/cmp-path",
-            "hrsh7th/cmp-emoji",
-            "hrsh7th/cmp-nvim-lua",
+            { "hrsh7th/cmp-calc", commit = "5947b412da67306c5b68698a02a846760059be2e" },
+            { "hrsh7th/cmp-path", commit = "c6635aae33a50d6010bf1aa756ac2398a2d54c32" },
+            { "hrsh7th/cmp-emoji", commit = "e8398e2adf512a03bb4e1728ca017ffeac670a9f" },
+            { "hrsh7th/cmp-nvim-lua", commit = "f12408bdb54c39c23e67cab726264c10db33ada8" },
             { "hrsh7th/cmp-nvim-lsp", commit = "a8912b88ce488f411177fc8aed358b04dc246d7b" },
 
             -- "L3MON4D3/LuaSnip",
--- .config/nvim/lua/niklas/lazy.lua
+++ .config/nvim/lua/niklas/lazy.lua
@@ -1,22 +1,12 @@
--- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.uv.fs_stat(lazypath) then
-    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-    if vim.v.shell_error ~= 0 then
-        vim.api.nvim_echo({
-            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-            { out,                            "WarningMsg" },
-            { "\nPress any key to exit..." },
-        }, true, {})
-        vim.fn.getchar()
-        os.exit(1)
-    end
+    return
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
     spec = {
+        { "folke/lazy.nvim", commit = "1c9ba3704564a2e34a22191bb89678680ffeb245" },
         { import = "niklas/lazy-plugins" },
     },
     rocks = { enabled = false },
