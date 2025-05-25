3254c6fb222053eef173c5d0c1ed93e8788c1e5e
Niklas Mohrin <dev@niklasmohrin.de>
2025-05-25 23:47:32 +0200

    More plugins

--- .config/nvim/lua/niklas/lazy-plugins/color.lua
+++ .config/nvim/lua/niklas/lazy-plugins/color.lua
@@ -1,6 +1,7 @@
 return {
     {
         "rmehri01/onenord.nvim",
+        commit = "039f76baf948acfc7c7d987ad9392fdc2a0e8a1c",
         lazy = false,
         priority = 1000,
         config = function()
--- .config/nvim/lua/niklas/lazy-plugins/statusline.lua
+++ .config/nvim/lua/niklas/lazy-plugins/statusline.lua
@@ -1,6 +1,7 @@
 return {
     {
         "feline-nvim/feline.nvim",
+        commit = "3587f57480b88e8009df7b36dc84e9c7ff8f2c49",
         priority = 999,
         config = function()
             local feline = require "feline"
