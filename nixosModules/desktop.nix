bce05c63398f80010af720eb9f1ed6ec1811bdc4
Niklas Mohrin <dev@niklasmohrin.de>
2025-08-07 00:19:43 +0200

    Silence xdg portals warning

--- nixosModules/desktop.nix
+++ nixosModules/desktop.nix
@@ -23,6 +23,7 @@
   xdg.portal = {
     enable = true;
     extraPortals = with pkgs; [ xdg-desktop-portal-gtk ];
+    config.common.default = "*"; # use first found portal implementation for everything (silence warning)
   };
   services.gvfs.enable = true; # file manager support for android phone
   programs.dconf.enable = true; # gnome theming thing (used for gtk theming)
